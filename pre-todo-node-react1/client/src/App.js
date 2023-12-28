0932ee73ba956990b74946a4ec0e74250e208511
ChangGyu <100547448+EotGGa@users.noreply.github.com>
2023-12-28 17:50:35 +0900

    .

--- pre-todo-node-react1/client/src/App.js
+++ pre-todo-node-react1/client/src/App.js
@@ -1,27 +1,15 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Footer from "./component/Footer";
+import Header from "./component/Header";
 
+const Home = () => <div>홈페이지</div>;
 export default function App() {
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("http://localhost:3007/api/todos")
-      .then((response) => setTodos(response.data))
-      .catch((error) => console.error("error발생", error));
-  }, []);
-
   return (
-    <div>
-      <h1>할일 리스트</h1>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo[0]}>
-            {todo[1]}
-            <li>{todo[2]}</li>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Router>
+      <div>
+        <Header />
+      </div>
+    </Router>
   );
 }
